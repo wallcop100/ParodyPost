497e5a7c2a10291956af3556898426e2c23ee98f
wallcop100 <github@wallcop.org>
2024-06-12 12:38:08 +0100

    Removed Client-Side Text Processing

--- src/main.py
+++ src/main.py
@@ -1,18 +1,18 @@
 import logging
-import pages
 from waveshare_epd import epd2in7_V2
 import time
 import requests
 import json
 import os
 from gpiozero import Button
+from PIL import Image
 
 # Set up logging
 logging.basicConfig(level=logging.DEBUG)
-
 LOCAL_JSON_PATH = 'local_satirical_content.json'
-REMOTE_JSON_URL = 'https://wallcop100.github.io/SatericalHeadlineBackend/satirical_content.json'
+REMOTE_JSON_URL = 'https://wallcop100.github.io/SatericalHeadlineBackend/output/manifest.json'
 CHECK_INTERVAL = 900  # 15 minutes in seconds
+epd = epd2in7_V2.EPD()
 
 # GPIO pins for the buttons
 BUTTON_PINS = [5, 6, 13, 19]
@@ -46,47 +46,27 @@ def save_local_json(path, data):
         logging.error(f"Error saving local JSON file: {e}")
 
 
-def render_title_page(epd, json_data):
-    date = json_data.get('Date', 'Unknown Date')
-    time_of_day = json_data.get('Time', 'Unknown Time')
-    politics_headline = json_data.get('Politics', {}).get('Headline', 'Default Politics Headline')
-    newline = '\n'
-    title_text = 'Parody Post'
-    date_text = f"{date} {newline} {time_of_day} Edition"
+def download_image(url, filename):
+    try:
+        response = requests.get(url, stream=True)
+        response.raise_for_status()
 
-    # Render and display the title page
-    pages.render_title_page(epd, title_text, date_text)
+        # Create content folder if it doesn't exist
+        if not os.path.exists("content"):
+            os.makedirs("content")
 
-
-def render_politics_headline(epd, json_data):
-    politics_headline = json_data.get('Politics', {}).get('Headline', 'Default Politics Headline')
-    # Render and display the title page
-    pages.render_headline_page(epd, politics_headline)
-
-
-
-def render_politics_page(epd, json_data):
-    politics_article = json_data.get('Politics', {}).get('Article', 'Default Politics Article')
-
-    # Render and display the politics page
-    pages.render_body_page(epd, politics_article)
+        filepath = os.path.join("content", filename)
+        with open(filepath, 'wb') as f:
+            for chunk in response.iter_content(1024):
+                f.write(chunk)
+        logging.info(f"Downloaded image: {filename}")
+    except requests.RequestException as e:
+        logging.error(f"Error downloading image: {url} - {e}")
 
 
-
-def render_general_page(epd, json_data):
-    general_headline = json_data.get('General', {}).get('Headline', 'Default General Headline')
-
-
-    # Render and display the general news page
-    pages.render_headline_page(epd, general_headline)
-
-
-
-def render_combined_page(epd, json_data):
-    general_article = json_data.get('General', {}).get('Article', 'Default General Article')
-    # Render and display the combined page
-    pages.render_body_page(epd, general_article)
-
+def render_page(page):
+    Render = Image.open(f'content/page_{page}.bmp')
+    epd.display(epd.getbuffer(Render))
 
 
 def button_callback(btn):
@@ -101,17 +81,13 @@ def button_callback(btn):
     if local_json_data:
         # Map each button to a specific page rendering function
         if btn.pin.number == 5:
-            #render_title_page(epd, local_json_data)
-            render_politics_headline(epd, local_json_data)
+            render_page(1)
         elif btn.pin.number == 6:
-            render_politics_page(epd, local_json_data)
+            render_page(2)
         elif btn.pin.number == 13:
-            render_general_page(epd, local_json_data)
+            render_page(3)
         elif btn.pin.number == 19:
-            render_combined_page(epd, local_json_data)
-
-
-
+            render_page(4)
 
 
 def setup_buttons():
@@ -119,9 +95,9 @@ def setup_buttons():
         btn = Button(pin)
         btn.when_pressed = button_callback
 
+
 def check_for_updates():
     logging.info("Starting Data Update Check")
-    epd = epd2in7_V2.EPD()
     while True:
         # Load local JSON data
         local_json_data = load_local_json(LOCAL_JSON_PATH)
@@ -134,21 +110,29 @@ def check_for_updates():
             if local_json_data != remote_json_data:
                 logging.info("New data found. Updating display.")
                 save_local_json(LOCAL_JSON_PATH, remote_json_data)
-                render_title_page(epd, remote_json_data)
+
+                # Clear content folder before downloading new images
+                for filename in os.listdir("content"):
+                    filepath = os.path.join("content", filename)
+                    os.remove(filepath)
+                    logging.info(f"Deleted old image: {filename}")
+
+                # Download images from Page Files section
+                page_files = remote_json_data["Page Files"]
+                for name, image_url in page_files.items():
+                    download_image(image_url, name)
+
+                render_page("title")
                 time.sleep(10)
-                render_politics_headline(epd, remote_json_data)
+                render_page(1)
             else:
                 logging.info("No new data. Skipping update.")
         else:
             logging.ERROR("Failed to fetch remote data. Using local data if available.")
             if local_json_data:
-                pages.render_headline_page(epd, "Failed to fetch remote data. Using local data if available.")
-                time.sleep(30)
-                render_politics_headline(epd, local_json_data)
-
+                render_page(1)
             else:
                 logging.CRITICAL("No local data available. Cannot update display.")
-                pages.render_headline_page(epd, "No local data available. Cannot update display.")
 
         # Sleep for the specified interval before checking again
         epd.sleep()
